e4613c9a7c878dd3f6c283c3e4b24c243230b999
Mike Jensen <mikejens@gmail.com>
2011-08-04 19:07:35 -0400

    Finally added full checking for required fields

--- filevalidator.py
+++ filevalidator.py
@@ -254,8 +254,54 @@ def streetsegCheck(tree, datafile):
 	ferr.write("Error Count: " + str(ecount))
 	fwarn.write("Warning Count: " + str(wcount))
 
-def fullrequiredCheck():
-	print "full required elements check"
+def getTags(elements):
+	tags = []
+	for e in elements:
+		tags.append(e.tag)
+	return tags
+
+def checkType(elem, elemtype, schema):
+	taglist = getTags(elem)
+	requiredList = schema[elemtype]["requireds"]
+	for k in range(len(requiredList)):
+		if not(requiredList[k] in taglist):
+			return False
+	return True
+
+def fullrequiredCheck(root, schema):
+	for vipelem in root:
+		ident = vipelem.get("id")
+		if schema["vip_object"][vipelem.tag]["indicator"] == "all":
+			children = vipelem.getchildren()
+			childrenTags = getTags(children)
+			requiredList = schema["vip_object"][vipelem.tag]["requireds"]
+			for i in range(len(schema["vip_object"][vipelem.tag]["elements"])):
+				if schema["vip_object"][vipelem.tag]["elements"][i]["name"] in requiredList:
+					if not(schema["vip_object"][vipelem.tag]["elements"][i]["name"] in childrenTags):
+						ferr.write("Error '" + vipelem.tag + "' ID:" + str(ident) + " missing " + schema["vip_object"][vipelem.tag]["elements"][i]["name"] + "\n")
+					elif schema["vip_object"][vipelem.tag]["elements"][i]["type"][0:3] != "xs:" and schema[schema["vip_object"][vipelem.tag]["elements"][i]["type"]]["type"] == "complexType":
+						for c in children:
+							if schema["vip_object"][vipelem.tag]["elements"][i]["name"] == c.tag:
+								if not(checkType(c,schema["vip_object"][vipelem.tag]["elements"][i]["type"],schema)):
+									ferr.write("Error '" + vipelem.tag + "' ID:" + str(ident) + " missing field in '" + schema["vip_object"][vipelem.tag]["elements"][i]["name"] + "'\n")
+								break
+		else:
+			children = vipelem.getchildren()
+			j = 0
+			for i in range(len(schema["vip_object"][vipelem.tag]["elements"])):
+				if j >= len(children):
+					if schema["vip_object"][vipelem.tag]["elements"][i]["required"] == "True":
+						ferr.write("Error '" + vipelem.tag + "' ID:" + str(ident) + " missing " + schema["vip_object"][vipelem.tag]["elements"][i]["name"]+"\n")
+						break 
+				elif schema["vip_object"][vipelem.tag]["elements"][i]["name"] == children[j].tag:
+					if schema["vip_object"][vipelem.tag]["elements"][i]["type"][0:3] != "xs:":
+						if not(checkType(children[j],schema["vip_object"][vipelem.tag]["elements"][i]["type"][0:3])):
+							ferr.write("Error '" + vipelem.tag + "' ID:" + str(ident) + " missing field in '" + schema["vip_object"][vipelem.tag]["elements"][i]["name"] + "'\n")
+							break
+					j+=1
+				elif schema["vip_object"][vipelem.tag]["elements"][i]["required"] == "True":
+					ferr.write("Error '" + vipelem.tag + "' ID:" + str(ident) + " missing " + schema["vip_object"][vipelem.tag]["elements"][i]["name"] + "\n")
+					break
 
 if results.version:
 	version = results.version
@@ -301,4 +347,8 @@ ferr.close()
 fwarn.close()
 
 if not(basicCheck):
-	fullrequiredCheck()
+	print "Running full check on required xml fields"
+	ferr = open(fname + "fullerrors.err","w")
+	fullrequiredCheck(root, parsedschema)
+	print "Finished full required xml field check, data located in " + fname + "fullerrors.err"
+	ferr.close()
