5d0208bd6fe02a039d6627e64d1a07ffa0171fa1
Mike Jensen <mikejens@gmail.com>
2011-10-25 17:21:19 -0400

    Now allows command line parameter of a directory and also will decompress all archived files correctly, even embedded ones

--- filevalidator.py
+++ filevalidator.py
@@ -5,6 +5,24 @@ from lxml import etree
 from streetsegmentvalidator import streetsegCheck
 from semanticvalidator import semanticCheck
 from fullrequiredvalidator import fullrequiredCheck
+import magic
+import rarfile
+import tarfile
+import gzip
+import zipfile
+import bz2
+import glob
+
+EXTRACT_PATH = "extracted"
+BASESCHEMAURL = "http://election-info-standard.googlecode.com/files/vip_spec_v"
+VERSIONLIST = ["2.0","2.1","2.2","2.3","3.0"]
+SIZELIMIT = 500000000
+FEED_DIR = "/home/votinginfoproject/feeds/"
+
+m = magic.Magic()
+
+xmlparser = etree.XMLParser()
+version = "3.0"
 
 def get_parsed_args():
 
@@ -16,24 +34,63 @@ def get_parsed_args():
 	parser.add_argument('-v', action='store', dest='version',
                         help='xsd version used for validation')
 
+	parser.add_argument('-d', action='store', dest='directory',
+			help='directory to search files for')
+	
 	parser.add_argument('-f', action='append', dest='files',
                         default=[], help='files to validate',)
 
 	return parser.parse_args()
 
-BASESCHEMAURL = "http://election-info-standard.googlecode.com/files/vip_spec_v"
-VERSIONLIST = ["2.0","2.1","2.2","2.3","3.0"]
-SIZELIMIT = 500000000
-FEED_DIR = "/home/votinginfoproject/feeds/"
+def find_largest(file_list):
+	large_name = ""
+	large_size = 0
+	for f in file_list:
+		size = os.path.getsize(f)
+		if size > large_size:
+			size = large_size
+			large_name = f
+	return large_name
 
-xmlparser = etree.XMLParser()
-version = "2.3"
+def check_file(path, fname):
+	ftype = m.from_file(fname)
+	if ftype.find("gzip") >= 0:
+		gz = gzip.GzipFile(fname, 'rb');
+		filedata = gz.read()
+		if not os.path.exists(path + "/" + EXTRACT_PATH):
+			os.makedirs(path + "/" + EXTRACT_PATH)
+		w = open(path + "/" + EXTRACT_PATH + "/feed_data","w")
+		w.write(filedata)
+	elif ftype.find("RAR") >= 0:
+		rf = rarfile.RarFile(fname)
+		rf.extractall(path=path+"/"+EXTRACT_PATH)
+	elif ftype.find("POSIX tar") >= 0:
+		tar = tarfile.open(fname)
+		tar.extractall(path=path+"/"+EXTRACT_PATH)
+	elif ftype.find("bzip2") >= 0:
+		bz = bz2.BZ2File(fname, 'rb');
+		filedata = bz.read()
+		if not os.path.exists(path + "/" + EXTRACT_PATH):
+			os.makedirs(path + "/" + EXTRACT_PATH)
+		w = open(path + "/" + EXTRACT_PATH + "/feed_data","w")
+		w.write(filedata)
+	elif ftype.find("Zip") >= 0:
+		zf = zipfile.ZipFile(fname)
+		zf.extractall(path=path+"/"+EXTRACT_PATH)
+	else:
+		return fname
+	if fname.find("extracted/") >= 0:
+		os.remove(fname)
+	flist = glob.glob(path + "/" + EXTRACT_PATH + "/*")
+	if len(flist) > 1:
+		check_file(find_largest(flist))
+	else:
+		check_file(flist[0])
 
 results = get_parsed_args()
 
 if results.version:
 	version = results.version
-
 if version == "2.2":
 	fschema = urllib.urlopen(BASESCHEMAURL + version + "a.xsd")
 else:
@@ -43,18 +100,31 @@ schema = schema.Schema(fschema)
 
 files = []
 
-if len(results.files) <= 0:
-        dirlist = os.listdir(FEED_DIR)
+if results.directory or len(results.files) <= 0:
+	if results.directory:
+		directory = results.directory
+	else:
+		directory = FEED_DIR
+
+        dirlist = os.listdir(directory)
         for folders in dirlist:
-                if os.path.isfile(FEED_DIR + folders) and folders.endswith(".xml"):
-                        files.append(FEED_DIR + folders)
-                elif os.path.isdir(FEED_DIR + folders):
-                        fnames = os.listdir(FEED_DIR + folders)
+                if os.path.isfile(directory + folders) and folders.endswith(".xml"):
+                        files.append(directory + folders)
+		elif os.path.isfile(directory + folders):
+			decompressed = check_file(directory, folders)
+			if decompressed.endswith(".xml"):
+				files.append(decompressed)
+                elif os.path.isdir(directory + folders):
+                        fnames = os.listdir(directory + folders)
                         for fname in fnames:
-                                path = FEED_DIR + folders + "/" + fname
+                                path = directory + folders + "/" + fname
                                 if os.path.isfile(path) and path.endswith(".xml"):
                                         files.append(path)
-else:
+				elif os.path.isfile(directory + folders):
+					decompressed = check_file(directory, folders)
+					if decompressed.endswith(".xml"):
+						files.append(decompressed)
+if len(results.files) > 0:
         fnames = results.files
         for f in fnames:
                 files.append(f)
