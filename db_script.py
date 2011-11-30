65266e0eed868d845bb9f4edc3d0eb4e828da2e3
Mike Jensen <mikejens@gmail.com>
2011-11-30 16:02:52 -0500

    Added Postgres to the db script options

--- db_script.py
+++ db_script.py
@@ -3,6 +3,7 @@ import schema
 import sqlite3
 import argparse
 import MySQLdb as mdb
+import psycopg2
 
 #default settings: 
 #	database_type:	sqlite3
@@ -12,7 +13,7 @@ import MySQLdb as mdb
 #	password:	password
 
 #cursor needs to be a script level variable since other mysqldb does not have 'execute script' we will have to execute every table create statement individually
-#TODO:Add connection stuff for postgres
+#TODO: Foreign keys? Maybe? eh
 
 def get_parsed_args():
 	parser = argparse.ArgumentParser(description='create database from schema')
@@ -34,7 +35,7 @@ def get_parsed_args():
 
 	return parser.parse_args()
 
-database_type = "sqlite3"
+database_type = "postgres"
 database_name = "vip"
 host = "localhost"
 username = "username"
@@ -54,19 +55,29 @@ data = schema.getElementData("element")["vip_object"]
 
 create_statement = ""
 
-TYPES = {"sqlite3":{"id":"TEXT", "xs:string":"TEXT", "xs:integer":"INTEGER", "xs:dateTime":"TEXT", "xs:date":"TEXT"}, "mysql":{"id":"VARCHAR(16)", "xs:string":"VARCHAR(256)", "xs:integer":"BIGINT", "xs:dateTime":"DATETIME", "xs:date":"DATE"}, "postgres":{"id":"VARCHAR(16)", "xs:string":"VARCHAR(256)", "xs:dateTime":"TIMESTAMP", "xs:date":"DATE"}} 
+TYPES = {"sqlite3":{"id":"TEXT", "xs:string":"TEXT", "xs:integer":"INTEGER", "xs:dateTime":"TEXT", "xs:date":"TEXT"}, "mysql":{"id":"VARCHAR(16)", "xs:string":"VARCHAR(256)", "xs:integer":"BIGINT", "xs:dateTime":"DATETIME", "xs:date":"DATE"}, "postgres":{"id":"VARCHAR(16)", "xs:string":"VARCHAR(256)", "xs:integer":"BIGINT", "xs:dateTime":"TIMESTAMP", "xs:date":"DATE"}} 
 
 if database_type == "sqlite3":
 	connection = sqlite3.connect("localhost")
 elif database_type == "mysql":
 	connection = mdb.connect(host, username, password, database_name)
+elif database_type == "postgres":
+	connection = psycopg2.connect(host=host, database=database_name, user=username, password=password)
 
 cursor = connection.cursor()
 
 #should rewrite this as a function accepting the elements, to create the original table and then the complex type tables
 
+def create_enums(data):
+	for element in data:
+		create_statement = "CREATE TYPE " + str(element) + " AS ENUM("
+		for e in data[element]["elements"]:
+			create_statement += "'" + e + "',"
+		create_statement = create_statement[:-1] + ");"
+		cursor.execute(create_statement)
+		connection.commit()	
+
 def element_create(data):
-	create_statement = ""
 	for element in data:
 		create_statement = "CREATE TABLE " + str(element["name"]) + " (id " + TYPES[database_type]["id"] + " PRIMARY KEY, element_id " + TYPES[database_type]["xs:integer"]
 		for e in element["elements"]:
@@ -84,22 +95,27 @@ def element_create(data):
 						for e_type in simple_types[e["type"]]["elements"]:
 							create_statement += "'" + e_type + "',"
 						create_statement = create_statement[:-1] + ")" #remove trailing ',' from ENUM
+					elif database_type == "postgres":
+						create_statement += " " + str(e["type"])
 				elif e["type"] in complex_types:
 					create_statement += ", " + str(e["name"]) + "_id " + TYPES[database_type]["xs:integer"]
 					#could add the foeign key stuff for postgres and mysql
 		create_statement += ");"		
 		print create_statement
 		cursor.execute(create_statement)		
+		if database_type == "postgres":
+			connection.commit()
 	return create_statement
 
 def complex_create(data):
-	create_statement = ""
 	for element in data:
 		create_statement = "CREATE TABLE " + str(element) + " (id " 
 		if database_type == "sqlite3":
 			create_statement += "INTEGER PRIMARY KEY AUTOINCREMENT"
 		elif database_type == "mysql":
 			create_statement += "BIGINT PRIMARY KEY AUTO_INCREMENT"
+		elif database_type == "postgres":
+			create_statement += "SERIAL PRIMARY KEY"
 		for e in data[element]["elements"]:
 			if e["name"] == "None":
 				continue
@@ -119,8 +135,12 @@ def complex_create(data):
 					create_statement += ", " + str(e["name"]) + "_id " + TYPES[database_type]["xs:integer"]
 		create_statement += ");"
 		cursor.execute(create_statement)		
+		if database_type == "postgres":
+			connection.commit()
 	return create_statement
 
+if database_type == "postgres":
+	create_enums(simple_types)
 element_create(data["elements"])
 complex_create(complex_types)
 
